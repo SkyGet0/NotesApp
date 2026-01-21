8774a91b670a8fd2a75cfca9917e1168228e03dd
SkyGet0 <239184206+SkyGet0@users.noreply.github.com>
2026-01-21 17:26:55 +0300

    created extensions class and added a data context into di container

---
+++ DataAccess/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services)
+        {
+            services.AddDbContext<AppContext>(x =>
+            {
+                x.UseNpgsql("Host=localhost;Database=NoteDb;Username=postgres;Password=1234");
+            });
+            return services;
+        }
+    }
+}
