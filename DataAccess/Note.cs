7546fd21f86878870899520fbdcde66dc55736e3
SkyGet0 <239184206+SkyGet0@users.noreply.github.com>
2026-01-22 00:16:21 +0300

    added a default String.Empty property to the Text field

--- DataAccess/Note.cs
+++ DataAccess/Note.cs
@@ -3,7 +3,7 @@
     public class Note
     {
         public int Id { get; set; }
-        public string Text { get; set; }
+        public string Text { get; set; } = string.Empty;
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
     }
