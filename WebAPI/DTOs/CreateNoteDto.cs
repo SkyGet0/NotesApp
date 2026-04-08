52530993d622232cd28901b373014520aec75cf3
SkyGet0 <239184206+SkyGet0@users.noreply.github.com>
2026-04-08 23:17:53 +0300

    Created a DTO for Note creation

---
+++ WebAPI/DTOs/CreateNoteDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.DTOs
+{
+    public class CreateNoteDto
+    {
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+        
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        public bool IsImportant { get; set; } 
+    }
+}
